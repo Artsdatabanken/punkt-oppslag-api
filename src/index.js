78f5d56a46bcb9f11e45753c15bd90975d2f6a28
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-08-10 11:52:07 +0200

    Fix quadrant calculation
    Note to self: Be careful when using multiple conditionals on one line

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ class Index {
       if (!tile) return r;
       r[z] = JSON.parse(tile.tile_data);
       r[z].key = key;
-      key += x > 0.5 ? 1 : 0 + y > 0.5 ? 2 : 0;
+      key += (x > 0.5 ? 1 : 0) + (y > 0.5 ? 2 : 0);
       x = 2 * (x % 0.5);
       y = 2 * (y % 0.5);
       z++;
