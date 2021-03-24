6bcfe38acbf302d357e5ccf5bc1e683b753e1a30
myrdrahl <even.juberg@gmail.com>
2021-03-24 14:04:04 +0100

    Legger til automatisk deploy til test.

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 docker build -t artsdatabanken/punkt-oppslag-api .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push artsdatabanken/punkt-oppslag-api
+curl -X POST -H 'Content-type: application/json' --data '{"text":"deploy punkt-oppslag-api"}' $slackaddy
