835ffa42e5aef948c7572b46a82af2658e462762
Myrdrahl <even.juberg@artsdatabanken.no>
2024-04-26 13:37:52 +0200

    fikser regex, grunnet endringer i xml-respons fra statkart

--- src/hoydeprofil.js
+++ src/hoydeprofil.js
@@ -15,13 +15,15 @@ async function diagram(points) {
     try {
         const callbackUrl = "https://punkt.test.artsdatabanken.no/v1/gpx?punkter=" + points
         const url = "http://openwps.statkart.no/skwms1/wps.elevation2?request=Execute&service=WPS&version=1.0.0&identifier=elevationChart&datainputs=gpx=@xlink:href=" + callbackUrl
+        console.log(url);
         const res = await fetch(url)
         console.log(res);
         const xml = await res.text()
         console.log(xml);
         const error = xml.match(/ExceptionText>(?<msg>.*?)\</)
         if (error) return { error: error.groups.msg }
-        const imgUrl = xml.match(/image\/png\">(?<img>.*?)\</).groups.img
+        const imgUrl = xml.match(/https:\/\/[^<]+\.png/)[0];
+        //const imgUrl = xml.match(/image\/png\">(?<img>.*?)\</).groups.img
         return { image: imgUrl }
     }
     catch (error) {
@@ -34,11 +36,15 @@ async function json(points) {
         const callbackUrl = "https://punkt.test.artsdatabanken.no/v1/gpx?punkter=" + points
         //const callbackUrl = "http://punkt.test.artsdatabanken.no/gpx.xml"
         const url = "http://openwps.statkart.no/skwms1/wps.elevation2?request=Execute&service=WPS&version=1.0.0&identifier=elevationJSON&datainputs=gpx=@xlink:href=" + callbackUrl
+        console.log(url);
         const res = await fetch(url)
+        console.log(res);
         const xml = await res.text()
+        console.log(xml);
         const error = xml.match(/ExceptionText>(?<msg>.*?)\</)
         if (error) return { error: error.groups.msg }
-        const jsonString = xml.match(/application\/json\">(?<img>.*?)\</).groups.img
+        const jsonString = xml.match(/<!\[CDATA\[(.*?)\]\]>/)[1];
+        //const jsonString = xml.match(/application\/json\">(?<img>.*?)\</).groups.img
         return jsonString
     }
     catch (error) {
